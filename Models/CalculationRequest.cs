08c6ac41edabd800fd9bdb351ba4a708bf1b0213
B3rtje123 <114138835+B3rtje123@users.noreply.github.com>
2022-09-29 10:23:09 +0200

    labo2, commit2

--- Models/CalculationRequest.cs
+++ Models/CalculationRequest.cs
@@ -1,7 +1,7 @@
 namespace MCT.Functions.Models;
 
 public class CalculationRequest{
-    public string Getal1{get; set;}
+    public int Getal1{get; set;}
     public string Operator {get; set;}
-    public string Getal2{get; set;}
+    public int Getal2{get; set;}
 }
